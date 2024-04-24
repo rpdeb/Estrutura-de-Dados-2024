0607749604945d802a35102b3ff3623a9881a201
Debora Ribeiro Pereira <52083218+rpdeb@users.noreply.github.com>
2024-04-24 11:43:28 -0300

    Create Principal.java

---
+++ Aula12/Principal.java
@@ -0,0 +1,11 @@
+package Aula12;
+
+public class Principal {
+    public static void main(String[] args) {
+        ArvoreBinaria arvore = new ArvoreBinaria();
+
+        arvore.inserirNo(arvore.noRaiz, 10);
+        arvore.inserirNo(arvore.noRaiz, 5);
+        arvore.inserirNo(arvore.noRaiz, 3);
+    }
+}
