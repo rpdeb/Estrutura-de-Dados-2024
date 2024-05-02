99f133e652f19fd3cad9d64e2f55aca2e8fb1f58
Debora Ribeiro Pereira <52083218+rpdeb@users.noreply.github.com>
2024-05-02 01:31:48 -0300

    Create ArvoreBinaria.java

---
+++ aula13/ArvoreBinaria.java
@@ -0,0 +1,25 @@
+package aula13;
+
+public class ArvoreBinaria {
+    public No noRaiz;
+
+    public ArvoreBinaria(){
+        this.noRaiz = null;
+    }
+    public void inserirNo(No noAtual, int elemento ){
+
+        if(this.noRaiz == null){
+            this.noRaiz = new No(elemento);
+        }
+        else if (elemento < noAtual.elemento)
+        {
+            if(noAtual.esquerda == null){
+                noAtual.esquerda = new No(elemento);
+            }
+            else{
+                noAtual = noAtual.esquerda;
+                inserirNo(noAtual, elemento);
+            }
+        }
+    }
+}
