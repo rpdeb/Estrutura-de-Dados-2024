9bcfbbe42c09d7afc14e9f95013078c7bbb81710
Debora Ribeiro Pereira <52083218+rpdeb@users.noreply.github.com>
2024-05-02 01:32:26 -0300

    Create No.java

---
+++ aula13/No.java
@@ -0,0 +1,13 @@
+package Aula13;
+
+public class No {
+    public int elemento;
+    public No direita;
+    public No esquerda;
+
+    public No(int elemento) {
+        this.elemento = elemento;
+        this.direita = null;
+        this.esquerda = null;
+    }
+}
